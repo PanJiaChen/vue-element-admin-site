4baf3651fa649e12721a152722f6e90c13a20772
Pan <panfree23@gmail.com>
2018-12-30 21:25:06 +0800

    update

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -2,7 +2,7 @@
   <Layout>
     <template slot="sidebar-top">
       <div :class="{'load-success':loadSuccess}">
-        <div id="codefund_ad" :key="$route.path"></div>
+        <div id="codefund" :key="$route.path"></div>
       </div>
     </template>
   </Layout>
@@ -10,13 +10,13 @@
 
 <script>
 import Layout from '@default-theme/Layout.vue'
+import axios from 'axios'
 
 export default {
   components: { Layout },
   data() {
     return {
-      loadSuccess: true,
-      hasLoaded: false
+      loadSuccess: true
     }
   },
   watch: {
@@ -24,26 +24,20 @@ export default {
       handler: function(val, oldVal) {
         const { path } = val
         if (path === '/zh/' || path === '/') return
-        if (!this.hasLoaded) {
-          this.addFundScript()
-        } else {
-          window._codefund && window._codefund.serve()
-        }
+        this.getCodefund()
       },
       immediate: true
     }
   },
   methods: {
-    addFundScript() {
+    getCodefund() {
       if (this.$isServer) return
-      const codefundId = this.isGitee()
-        ? 'c0e8a6c2-6717-402f-aea7-bfdcaaaf2329'
-        : 'c010d89c-46a8-4e3a-abf0-86b8a02874e4'
-      const script = window.document.createElement('script')
-      script.onerror = this.loadError
-      script.src = 'https://codefund.io/scripts/' + codefundId + '/embed.js'
-      document.body.appendChild(script)
-      this.hasLoaded = true
+      const codefundId = this.isGitee() ? '79' : '116'
+      axios
+        .get(`https://api.codefund.app/properties/${codefundId}/funder.html`)
+        .then(function(response) {
+          document.getElementById('codefund').innerHTML = response.data
+        })
     },
     isGitee() {
       const origin = window.location.origin
