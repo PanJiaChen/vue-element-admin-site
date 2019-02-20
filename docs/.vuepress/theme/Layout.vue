a2a46e076d96ec06c6753d54632a7cb68f8f6c14
Pan <panfree23@gmail.com>
2019-02-20 14:29:15 +0800

    update

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -1,21 +1,51 @@
 <template>
-  <Layout>
-    <template slot="sidebar-top">
-      <div :class="{'load-success':loadSuccess}">
+  <div
+    class="theme-container"
+    :class="pageClasses"
+    @touchstart="onTouchStart"
+    @touchend="onTouchEnd"
+  >
+    <Navbar v-if="shouldShowNavbar" @toggle-sidebar="toggleSidebar"/>
+
+    <div class="sidebar-mask" @click="toggleSidebar(false)"></div>
+    <Sidebar :items="sidebarItems" @toggle-sidebar="toggleSidebar">
+      <div slot="top" :class="{'load-success':loadSuccess}">
         <div id="codefund" :key="$route.path"></div>
       </div>
-    </template>
-  </Layout>
+      <slot name="sidebar-bottom" slot="bottom"/>
+    </Sidebar>
+
+    <div class="custom-layout" v-if="$page.frontmatter.layout">
+      <component :is="$page.frontmatter.layout"/>
+    </div>
+
+    <Home v-else-if="$page.frontmatter.home"/>
+
+    <Page v-else :sidebar-items="sidebarItems">
+      <slot name="page-top" slot="top"/>
+      <slot name="page-bottom" slot="bottom"/>
+    </Page>
+
+    <SWUpdatePopup :updateEvent="swUpdateEvent"/>
+  </div>
 </template>
 
 <script>
-import Layout from '@default-theme/Layout.vue'
+import Vue from 'vue'
+import nprogress from 'nprogress'
+import Home from './Home.vue'
+import Navbar from '@default-theme/Navbar.vue'
+import Page from '@default-theme/Page.vue'
+import Sidebar from '@default-theme/Sidebar.vue'
+import SWUpdatePopup from '@default-theme/SWUpdatePopup.vue'
+import { resolveSidebarItems } from '@default-theme/util'
 import axios from 'axios'
-
 export default {
-  components: { Layout },
+  components: { Home, Page, Sidebar, Navbar, SWUpdatePopup },
   data() {
     return {
+      isSidebarOpen: false,
+      swUpdateEvent: null,
       loadSuccess: true
     }
   },
@@ -24,17 +54,85 @@ export default {
       handler: function(val, oldVal) {
         const { path } = val
         if (path === '/zh/' || path === '/') return
+        // if (this.isCN) return
         this.getCodefund()
       },
       immediate: true
     }
   },
+  computed: {
+    isCN() {
+      return this.$lang === 'zh-CN'
+    },
+    shouldShowNavbar() {
+      const { themeConfig } = this.$site
+      const { frontmatter } = this.$page
+      if (frontmatter.navbar === false || themeConfig.navbar === false) {
+        return false
+      }
+      return (
+        this.$title ||
+        themeConfig.logo ||
+        themeConfig.repo ||
+        themeConfig.nav ||
+        this.$themeLocaleConfig.nav
+      )
+    },
+    shouldShowSidebar() {
+      const { frontmatter } = this.$page
+      return (
+        !frontmatter.layout &&
+        !frontmatter.home &&
+        frontmatter.sidebar !== false &&
+        this.sidebarItems.length
+      )
+    },
+    sidebarItems() {
+      return resolveSidebarItems(
+        this.$page,
+        this.$route,
+        this.$site,
+        this.$localePath
+      )
+    },
+    pageClasses() {
+      const userPageClass = this.$page.frontmatter.pageClass
+      return [
+        {
+          'no-navbar': !this.shouldShowNavbar,
+          'sidebar-open': this.isSidebarOpen,
+          'no-sidebar': !this.shouldShowSidebar
+        },
+        userPageClass
+      ]
+    }
+  },
+  mounted() {
+    console.log(this)
+    window.addEventListener('scroll', this.onScroll)
+    // configure progress bar
+    nprogress.configure({ showSpinner: false })
+    this.$router.beforeEach((to, from, next) => {
+      if (to.path !== from.path && !Vue.component(to.name)) {
+        nprogress.start()
+      }
+      next()
+    })
+    this.$router.afterEach(() => {
+      nprogress.done()
+      this.isSidebarOpen = false
+    })
+    this.$on('sw-updated', this.onSWUpdated)
+  },
   methods: {
     getCodefund() {
+      console.log('aa')
       if (this.$isServer) return
       const codefundId = this.isGitee() ? '79' : '116'
       axios
-        .get(`https://api.codefund.app/properties/${codefundId}/funder.html`)
+        .get(
+          `https://api.codefund.app/properties/${codefundId}/funder.html?template`
+        )
         .then(function(response) {
           document.getElementById('codefund').innerHTML = response.data
         })
@@ -46,19 +144,45 @@ export default {
       }
       return false
     },
-
     loadError(oError) {
       this.loadSuccess = false
+    },
+    toggleSidebar(to) {
+      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
+    },
+    // side swipe
+    onTouchStart(e) {
+      this.touchStart = {
+        x: e.changedTouches[0].clientX,
+        y: e.changedTouches[0].clientY
+      }
+    },
+    onTouchEnd(e) {
+      const dx = e.changedTouches[0].clientX - this.touchStart.x
+      const dy = e.changedTouches[0].clientY - this.touchStart.y
+      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
+        if (dx > 0 && this.touchStart.x <= 80) {
+          this.toggleSidebar(true)
+        } else {
+          this.toggleSidebar(false)
+        }
+      }
+    },
+    onSWUpdated(e) {
+      this.swUpdateEvent = e
     }
   }
 }
 </script>
 
+<style src="prismjs/themes/prism-tomorrow.css"></style>
+<style src="@default-theme/styles/theme.styl" lang="stylus"></style>
+
 <style>
-#cf_ad .cf-text {
-  display: inline !important;
+.cf-wrapper a {
+  display: block;
 }
-.load-success {
-  min-height: 130px;
+.sidebar .sidebar-links {
+  padding-top: 10px;
 }
 </style>
